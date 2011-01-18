ed0c9d035561d27cea175df4af92802edd900f57
noobnl <shareefalis@gmail.com>
2011-01-17 18:56:48 -0600

    add cyanogenmod7 based info

--- products/cyanogen_epic.mk
+++ products/cyanogen_epic.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := epic
 PRODUCT_MODEL := Samsung-SPH-D700
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 TARGET_DEVICE=SPH-D700 TARGET_BOOTLOADER_BOARD_NAME=SPH-D700 BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 TARGET_DEVICE=SPH-D700 TARGET_BOOTLOADER_BOARD_NAME=SPH-D700 BUILD_ID=GRH78 BUILD_DISPLAY_ID=GRH78 BUILD_FINGERPRINT=google/soju/crespo:2.3.1/GRH78/85442:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.1 GRH78 85442 release-keys"
 
 # Extra Galaxy S overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/galaxys
@@ -25,10 +25,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/galaxys
 #
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Epic
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Epic
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.2.0-BETA0-Epic
+        ro.modversion=CyanogenMod-7-BETA0-Epic
 endif
 
 #
