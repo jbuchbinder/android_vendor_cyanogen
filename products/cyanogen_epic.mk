ebadb50dde42d4a2094bbc88fcb20aeb9b18259d
noobnl <shareefalis@gmail.com>
2011-01-22 13:41:38 -0600

    merged changes

--- products/cyanogen_epic.mk
+++ products/cyanogen_epic.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/samsung/epic/full_epic.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
@@ -15,7 +15,7 @@ PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := epic
 PRODUCT_MODEL := Samsung-SPH-D700
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 TARGET_DEVICE=SPH-D700 TARGET_BOOTLOADER_BOARD_NAME=SPH-D700 BUILD_ID=GRH78 BUILD_DISPLAY_ID=GRH78 BUILD_FINGERPRINT=google/soju/crespo:2.3.1/GRH78/85442:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.1 GRH78 85442 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 TARGET_DEVICE=SPH-D700 TARGET_BOOTLOADER_BOARD_NAME=SPH-D700 BUILD_ID=GRH78 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/soju/crespo:2.3.1/GRH78/85442:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.1 GRH78 85442 release-keys"
 
 # Extra Galaxy S overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/galaxys
